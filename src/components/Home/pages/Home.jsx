6bd904eae18c757d6d39f2e5de5dbdac79e23aee
johnny.thai <johnny.thai@CP15025-JOHNNY>
2022-05-13 16:07:09 +0700

    feat: home

--- src/components/Home/pages/Home.jsx
+++ src/components/Home/pages/Home.jsx
@@ -4,79 +4,59 @@ import './Home.scss';
 const Home = () => {
   return (
     <div>
-      <h1 class="section-title">
-        SC<span>A</span>DA
+      <h1 className="section-title">
+        SC<span>A</span>D<span>A</span>
       </h1>
-      <div class="container bootstrap snippets bootdeys scada">
-        <div class="row">
-          <div class="col-md-3 col-sm-6 content-card">
-            <div class="card-big-shadow">
-              <div class="card card-just-text" data-background="color" data-color="blue" data-radius="none">
-                <div class="content">
-                  <h6 class="category">Best cards</h6>
-                  <h4 class="title">
-                    <a href="/">Blue Card</a>
-                  </h4>
-                  <p class="description">
-                    What all of these have in common is that they're pulling information out of the app or the service
-                    and making it relevant to the moment.{' '}
-                  </p>
+      <div className="container bootstrap snippets bootdeys scada">
+        <div className="row">
+          <div className="col-md-4">
+            <div className="card-big-shadow">
+              <div className="card card-just-text" data-background="color" data-color="blue" data-radius="none">
+                <div className="content">
+                  <h3 className="title-card">Trạng thái của cây</h3>
+                  <div className="content-card-tree">
+                    <h4>Chiều cao:</h4>
+                    <p className="description">168 cm</p>
+                  </div>
+                  <div className="content-card-tree">
+                    <h4>Màu lá:</h4>
+                    <p className="description">Xanh</p>
+                  </div>
                 </div>
               </div>
             </div>
           </div>
 
-          <div class="col-md-3 col-sm-6 content-card">
-            <div class="card-big-shadow">
-              <div class="card card-just-text" data-background="color" data-color="green" data-radius="none">
-                <div class="content">
-                  <h6 class="category">Best cards</h6>
-                  <h4 class="title">
-                    <a href="/">Green Card</a>
-                  </h4>
-                  <p class="description">
-                    What all of these have in common is that they're pulling information out of the app or the service
-                    and making it relevant to the moment.{' '}
-                  </p>
+          <div className="col-md-4 ">
+            <div className="card-big-shadow">
+              <div className="card card-just-text" data-background="color" data-color="yellow" data-radius="none">
+                <div className="content">
+                  <h3 className="title-card">Nhiệt độ</h3>
+                  <div className="content-card-tree">
+                    <p className="temp">30°C</p>
+                  </div>
+                  <div className="content-card-tree">
+                    <p className="temp"> Cảnh báo mức cao</p>
+                  </div>
                 </div>
               </div>
             </div>
           </div>
-
-          <div class="col-md-3 col-sm-6 content-card">
-            <div class="card-big-shadow">
-              <div class="card card-just-text" data-background="color" data-color="yellow" data-radius="none">
-                <div class="content">
-                  <h6 class="category">Best cards</h6>
-                  <h4 class="title">
-                    <a href="/">Yellow Card</a>
-                  </h4>
-                  <p class="description">
-                    What all of these have in common is that they're pulling information out of the app or the service
-                    and making it relevant to the moment.{' '}
-                  </p>
+          <div className="col-md-4">
+            <div className="card-big-shadow">
+              <div className="card card-just-text" data-background="color" data-color="brown" data-radius="none">
+                <div className="content">
+                  <h3 className="title-card">Độ ẩm</h3>
+                  <div className="content-card-tree">
+                    <p className="humidity">78%</p>
+                  </div>
+                  <div className="content-card-tree">
+                    <p className="humidity"> Cảnh báo mức cao</p>
+                  </div>
                 </div>
               </div>
             </div>
           </div>
-
-          <div class="col-md-3 col-sm-6 content-card">
-            <div class="card-big-shadow">
-              <div class="card card-just-text" data-background="color" data-color="brown" data-radius="none">
-                <div class="content">
-                  <h6 class="category">Best cards</h6>
-                  <h4 class="title">
-                    <a href="/">Brown Card</a>
-                  </h4>
-                  <p class="description">
-                    What all of these have in common is that they're pulling information out of the app or the service
-                    and making it relevant to the moment.{' '}
-                  </p>
-                </div>
-              </div>
-            </div>
-          </div>
-
         </div>
       </div>
     </div>
