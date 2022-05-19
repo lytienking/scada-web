06e722f3352b544fbae72b818afddb06e190da9c
johnny.thai <johnny.thai@CP15025-JOHNNY>
2022-05-19 11:37:28 +0700

    update home page

---
+++ src/api/scadaApi.js
@@ -0,0 +1,10 @@
+import axiosClient from "./axiosClient";
+
+const scadaApi = {
+  update: (params) => {
+    const url = "";
+    return axiosClient.post(url, params);
+  }
+};
+
+export default scadaApi;
--- src/components/Home/pages/Home.jsx
+++ src/components/Home/pages/Home.jsx
@@ -1,7 +1,18 @@
 import * as React from 'react';
+import scadaApi from '../../../api/scadaApi';
 import './Home.scss';
 
 const Home = () => {
+  const clickMe = async () => {
+    let params = {
+      name: 'Johnny',
+      value: 'test6',
+      datetime: '2022-05-10',
+    };
+    const res = await scadaApi.update(params);
+    console.log(res);
+  };
+
   return (
     <div>
       <h1 className="section-title">
@@ -56,6 +67,50 @@ const Home = () => {
                 </div>
               </div>
             </div>
+          </div>
+        </div>
+      </div>
+      <div className="container bootstrap snippets bootdeys scada">
+        <div className="row">
+          <div className="form-style-2">
+            <div className="form-style-2-heading">Bảng điều khiển</div>
+            <form action="" method="post">
+              <label htmlFor="field1">
+                <span>
+                  Chiều cao <span className="required">*</span>
+                </span>
+                <input type="text" placeholder='Nhập (0-100)' className="input-field" name="field1" />
+              </label>
+              <label htmlFor="field4">
+                <span>
+                  Màu lá <span className="required">*</span>
+                </span>
+                <select name="field4" className="select-field">
+                  <option value="General Question">Xanh</option>
+                  <option value="Advertise">Héo</option>
+                  <option value="Partnership">Vàng</option>
+                </select>
+              </label>
+              <label htmlFor="field2">
+                <span>
+                  Nhiệt độ <span className="required">*</span>
+                </span>
+                <input type="text" placeholder='Nhập (20-80)' className="input-field" name="field2" />
+              </label>
+              <label htmlFor="field2">
+                <span>
+                  Độ ẩm <span className="required">*</span>
+                </span>
+                <input type="text" placeholder='Nhập (0-100)' className="input-field" name="field2" />
+              </label>
+              <label htmlFor="field2">
+                <span>
+                  Bơm DC <span className="required">*</span>
+                </span>
+                <input type="text" placeholder='Nhập (1-60)' className="input-field" name="field2" />
+              </label>
+            </form>
+            <button  onClick={clickMe}> Xác nhận</button>
           </div>
         </div>
       </div>
